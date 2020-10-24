e013acd3eb88b80b49295e676b59e1604cfb68c1
hailing045 <72068271+hailing045@users.noreply.github.com>
2020-10-24 20:06:30 +0100

    update the test

--- LCA.java
+++ LCA.java
@@ -37,7 +37,6 @@ public class LCA {
 		int i; 
 		for (i = 0; i < path1.size() && i < path2.size(); i++) { 
 			
-		// System.out.println(path1.get(i) + " " + path2.get(i)); 
 			if (!path1.get(i).equals(path2.get(i))) 
 				break; 
 		} 
@@ -77,19 +76,4 @@ public class LCA {
 		return false; 
 	} 
 
-	// Driver code 
-	public static void main(String[] args) 
-	{ 
-		LCA tree = new LCA(); 
-		tree.root = new Node(1); 
-		tree.root.left = new Node(2); 
-		tree.root.right = new Node(3); 
-		tree.root.left.left = new Node(4); 
-		tree.root.left.right = new Node(5); 
-		tree.root.right.left = new Node(6); 
-		tree.root.right.right = new Node(7); 
-
-		System.out.println("LCA of 4 and 5 is " + tree.findLCA(4,5)); 
-	
-	} 
 }
--- LCATest.java
+++ LCATest.java
@@ -9,35 +9,65 @@ import java.util.Arrays;
 
 
 public class LCATest {
-
 	@Test
-	public void findLCA() {
+	public void testConstructor() {
+		//Construct a node
+		Node node = new Node(2);
+		assertEquals(2, node.data);
+		//the left and right leaf for it are null
+		assertEquals(null, node.left);
+		assertEquals(null, node.right);
+	}
+	
+	@Test
+	public void testfindLCA() {
 	//create a tree
 		LCA tree = new LCA(); 
-	//when tree is empty
+		
+	//test the false case in private method findLCAInternal
+	
+		//when tree is empty
+	// tree is empty therefore return -1
 		int lca0 = tree.findLCA(4,5);
 		assertEquals(-1, lca0);
+	
+	//test the true case for private method findLCAInternal
+	//and method findPath
+		
+	//when tree only has root
+	/*			1
+	 */		
+		tree.root = new Node(1); 
+		assertEquals(-1, tree.findLCA(4,5));
+		assertEquals(-1, tree.findLCA(0,1));
+		
+	//when tree has three nodes
+	/*			1
+	 * 	 	   / \
+	 * 	      2   3
+	*/	
+		tree.root.left = new Node(2); 
+		tree.root.right = new Node(3);
+		
+		assertEquals(-1, tree.findLCA(4,5));
+		assertEquals(1, tree.findLCA(2,3));
+		
 	//when tree has value
 	/*			1
 	 *'		   / \
 	 * 	      2   3
 	 * 		 /\   /\
 	 *      4  5 6  7
-	 * 
 	 * */
-		tree.root = new Node(1); 
-		tree.root.left = new Node(2); 
-		tree.root.right = new Node(3); 
 		tree.root.left.left = new Node(4); 
 		tree.root.left.right = new Node(5); 
 		tree.root.right.left = new Node(6); 
 		tree.root.right.right = new Node(7); 
-
-		int lca1 = tree.findLCA(4,5);
-		assertEquals(2, lca1);
-		int lca2 = tree.findLCA(4,7);
-		assertEquals(1, lca2);
 		
+		assertEquals(-1, tree.findLCA(2,0));
+		assertEquals(2, tree.findLCA(4,5));
+		assertEquals(1, tree.findLCA(4,7));
+		assertEquals(3, tree.findLCA(6,7));
 	}
 
 }
